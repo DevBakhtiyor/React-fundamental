c2b965f6f8e2b28be4d387aeee00a25716b2f1f8
DevBakhtiyor <bakhtiyor.dev.it@gmail.com>
2022-12-26 19:27:31 -0500

    Learing React Fundamental

--- src/index.js
+++ src/index.js
@@ -1,17 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// function Greeting() {
+//   return <h2>My First Component</h2>;
+// }
+// function Greeting() {
+//   return React.createElement('h2', {}, 'hello world');
+// }
+// function Greeting() {
+//   return (
+//     <div>
+//       <h2>My First Component</h2>
+//     </div>
+//   );
+// }
+// function Greeting() {
+//   return (
+//     <div>
+//       <div>
+//         <h3>hello people</h3>
+//         <ul>
+//           <li>
+//             <a href="#">hello world</a>
+//           </li>
+//         </ul>
+//       </div>
+//       <h2>hello world</h2>
+//     </div>
+//   );
+// }
+// function Greeting() {
+//   return (
+//     <React.Fragment>
+//       <div>
+//         <h3>hello people</h3>
+//         <ul>
+//           <li>
+//             <a href="#">hello world</a>
+//           </li>
+//         </ul>
+//       </div>
+//       <h2>hello world</h2>
+//     </React.Fragment>
+//   );
+// }
+// function Greeting() {
+//   return (
+//     <>
+//       <div className="someValue">
+//         <h3>hello people</h3>
+//         <ul>
+//           <li>
+//             <a href="#">hello world</a>
+//           </li>
+//         </ul>
+//       </div>
+//       <h2>hello world</h2>
+//       <input type="text" name="" id="" />
+//     </>
+//   );
+// }
+function Greeting() {
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  );
+}
+const Person = () => <h2>john doe</h2>;
+const Message = () => {
+  return <p>this is my message</p>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Greeting />);
